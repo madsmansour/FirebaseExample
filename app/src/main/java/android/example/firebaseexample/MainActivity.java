069ca21b139efcc2751c283b61e4368b15232230
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-04-26 12:35:13 +0200

    Firebase example

--- app/src/main/java/android/example/firebaseexample/MainActivity.java
+++ app/src/main/java/android/example/firebaseexample/MainActivity.java
@@ -1,19 +1,29 @@
 package android.example.firebaseexample;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference databaseReference;
     EditText editText;
     Button button;
+    private ChildEventListener childEventListener;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.edit);
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
         databaseReference = FirebaseDatabase.getInstance().getReference().child("text");
 
@@ -31,5 +42,41 @@ public class MainActivity extends AppCompatActivity {
                 editText.setText("");
             }
         });
+        attachDatabaseReadListener();
+
+    }
+
+    private void attachDatabaseReadListener() {
+        if (childEventListener == null) {
+            childEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    String text = dataSnapshot.getValue().toString();
+                    System.out.println(dataSnapshot.toString());
+                    textView.append(text.toString() + "\n");
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+        }
+        databaseReference.addChildEventListener(childEventListener);
     }
 }
